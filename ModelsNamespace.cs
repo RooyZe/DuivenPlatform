4cfc66ea50130353d0c1c71c540f94d66f976d92
Roy <roytencate13@hotmail.com>
2026-04-13 15:03:20 +0200

    Home/Duiven/Vluchten/Navigatie pagina aangemaakt

---
+++ ModelsNamespace.cs
@@ -0,0 +1,8 @@
+namespace DuivenPlatform.Web.Models
+{
+    // Marker class to ensure the DuivenPlatform.Web.Models namespace exists
+    // so Razor views can reference it via @using DuivenPlatform.Web.Models
+    internal static class ModelsNamespaceMarker
+    {
+    }
+}
